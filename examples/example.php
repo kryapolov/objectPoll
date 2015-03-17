f41411986053f410cfe5abc313e32fa1b7d1b6bf
Konstantin Ryapolov <kryapolov@gollard.ru>
2015-03-17 18:53:53 +0400

    add example UsCase for ObjectPoll

---
+++ examples/example.php
@@ -0,0 +1,51 @@
+#!/usr/bin/env php
+<?php
+require_once('lib/ObjectPoll/autoload.php');
+
+use ObjectPoll\ObjectPool;
+
+trait internalUses
+{
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+
+class A101
+{
+
+    use internalUses;
+
+    private $name;
+
+    public function __construct()
+    {
+        $this->name = __CLASS__;
+    }
+
+}
+
+class A102
+{
+    private $name;
+    use internalUses;
+
+    public function __construct()
+    {
+        $this->name = __CLASS__;
+    }
+
+}
+
+
+$pool = new ObjectPool();
+
+$pool->getByKeyRecursive("A101");
+$pool->getByKeyRecursive("A102");
+$pool->getByKeyRecursive("A101");
+$pool->getByKeyRecursive("A102");
+
+echo "number of objects in the pool: ".$pool->getPoolSize()."\n\r";
+
