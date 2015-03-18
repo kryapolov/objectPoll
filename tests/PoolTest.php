550d68b9564aed0ed66ffa1e332ce93f8e04fbc1
Konstantin Ryapolov <kryapolov@gollard.ru>
2015-03-18 15:18:04 +0400

    #1 add test for method getByKeyRecursive

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -2,6 +2,9 @@
 
 namespace ObjectPool\tests;
 use ObjectPool\ObjectPool;
+use ObjectPool\tests\stubs\ExampleOne;
+use ObjectPool\tests\stubs\ExampleTwo;
+
 
 /**
  * Class PoolTest
@@ -16,14 +19,40 @@ class PoolTest extends \PHPUnit_Framework_TestCase {
     public function testSizeHasBeSet()
     {
 
-        $poll = $pool = new ObjectPool();
+        $pool = $pool = new ObjectPool();
 
-        $poll->setMaxPoolSize($this::MAX_POLL_SIZE);
+        $pool->setMaxPoolSize($this::MAX_POLL_SIZE);
         // Act
 
 
         // Assert
-        $this->assertEquals($this::MAX_POLL_SIZE, $poll->getMaxPoolSize());
+        $this->assertEquals($this::MAX_POLL_SIZE, $pool->getMaxPoolSize());
+
+    }
+
+    /**
+     * 2 write object to pull a few times and check the size of the pool
+     */
+    public function testGetByKeyRecursive()
+    {
+
+        $exampleOne = new ExampleOne();
+        $keyOfExampleOne = $exampleOne->getName();
+
+        $exampleTwo = new ExampleTwo();
+        $keyOfExampleTwo = $exampleTwo->getName();
+
+        $pool = new ObjectPool();
+
+        $pool->getByKeyRecursive($keyOfExampleOne);
+        $pool->getByKeyRecursive($keyOfExampleTwo);
+        $firstSize = $pool->getPoolSize();
+
+        // save dublicates to pool
+        $pool->getByKeyRecursive($keyOfExampleOne);
+        $pool->getByKeyRecursive($keyOfExampleTwo);
+
+        $this->assertEquals($firstSize, $pool->getPoolSize());
 
     }
 
